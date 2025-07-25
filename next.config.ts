c3434d695e6987f4e8af680f3f8cb3f9bff512a4
Wordiii <wordiii777@gmail.com>
2025-07-25 03:41:34 +0330

    i

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: "/Pishraft",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
